6c5c5fa9a2694a3a43c5f0faef7bfad7ce9f3722
Hades Achahuanco <jea.haa@gmail.com>
2021-11-24 09:59:10 -0500

    agrgar html y main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+import data from './data/ghibli/ghibli.js';
+
+const showMovies = (movies)=> {
+    const divMovie = document.getElementById('movies');
+    
+    movies.films.forEach(movie => {
+        let newImg = document.createElement("img");
+        newImg.src = movie.poster;
+        divMovie.appendChild(newImg);
+        console.log(movie.poster)});
+    //console.log(movies.films);    
+    
+}
+
+showMovies(data);
+
+
