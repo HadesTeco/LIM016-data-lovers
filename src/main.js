90575e4ac5cc254ec3ef9949f485d45f10b923bc
Hades Achahuanco <jea.haa@gmail.com>
2021-12-07 09:06:11 -0500

    Agregar html, js, css

--- src/main.js
+++ src/main.js
@@ -1,19 +1,54 @@
-import { example } from './data.js';
+import { example} from './data.js';
 
 import data from './data/ghibli/ghibli.js';
 
+//console.log(data.films[0].people[0].img);
+
 const showMovies = (movies)=> {
     const divMovie = document.getElementById('movies');
-    
-    movies.films.forEach(movie => {
+        movies.films.forEach(movie => {
         let newImg = document.createElement("img");
         newImg.src = movie.poster;
         divMovie.appendChild(newImg);
-        console.log(movie.poster)});
-    //console.log(movies.films);    
-    
+        //console.log(newImg)       
+    });  
 }
-
 showMovies(data);
 
+const showCharacters = (all)=> {
+   const divPersonajes = document.getElementById("personajes");
+    all.films.forEach(movie =>{
+        movie.people.forEach(character=> {
+        let newPerson = document.createElement("img");
+        newPerson.src = character.img
+        //console.log(newPerson)
+        divPersonajes.appendChild(newPerson);
+    });
+ });
+}
+showCharacters(data);
+
+const showLocations = (all)=> {
+    const divLocaciones = document.getElementById("locaciones");
+    all.films.forEach(movie =>{
+        movie.locations.forEach(locacionesT=> {
+        let newLocation = document.createElement("img");
+        newLocation.src = locacionesT.img
+        //console.log(newLocation)
+        divLocaciones.appendChild(newLocation);
+    });
+ });
+}
+showLocations(data);
+
+const showDirectors = (all)=> {
+    const divDirectors = document.getElementById('directores');
+    all.films.forEach(movie => {
+        let newDirectors = document.createElement("txt");
+        newDirectors = movie.director;
+        divDirectors.appendChild(newDirectors);
+        console.log(newDirectors);       
+    });  
+}
+showDirectors(data);
 
